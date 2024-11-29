5e6ac5aabb033060c5a6c9cdecd6d4761164378f
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2024-11-29 08:46:07 +0100

    classe noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const body = document.body;
 
+
 toggle.addEventListener("click", () => {
     const isOpen = toggle.ariaExpanded === "true";
 const isClosed = !isOpen;
